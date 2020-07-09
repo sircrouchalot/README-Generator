2a0cf2cb5f6c38e5627e60cce18955662264bfc3
Robert Alex Crouch <acrouch06@gmail.com>
2020-07-09 16:28:00 -0500

    able to write to file with md format

--- index.js
+++ index.js
@@ -51,6 +51,58 @@ inquirer
     }
   ])
   .then(function (response) {
-    console.log(response.project);
-    console.log(response.email);
+    const { project, description, installation, usage, contributing, tests, license, username, email } = response;
+    
+    var readmeContent = `# ${project}
+    ## Table of Contents
+    
+    1. Description
+    2. Installation
+    3. Usage
+    4. Contributing
+    5. Tests
+    6. License
+    7. Contact
+    
+    ## Description
+    
+    ${description}
+    
+    ## Installation
+    
+    \`\`\`
+    ${installation}
+    \`\`\`
+    
+    ## Usage
+    
+    ${usage}
+    
+    ## Contributing
+    
+    ${contributing}
+    
+    ## Tests
+
+    \`\`\`
+    ${tests}
+    \`\`\`
+
+    ## License
+    
+    ${license}
+    
+    ## Contact
+    
+    GitHub Username: ${username}
+    Email: ${email}
+    `
+
+    fs.writeFile("./assets/README.md", readmeContent, function(err) {
+      if (err) {
+        throw err;
+      }
+
+
+    })
   });
