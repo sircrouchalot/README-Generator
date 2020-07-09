7324b2665279b8c3426a4487dc31eae7eb0cbda3
Robert Alex Crouch <acrouch06@gmail.com>
2020-07-09 18:23:59 -0500

    mad table of contents clickable

--- index.js
+++ index.js
@@ -53,50 +53,55 @@ inquirer
   .then(function (response) {
     const { project, description, installation, usage, contributing, tests, license, username, email } = response;
     
-    var readmeContent = `# ${project}
-    ## Table of Contents
-    
-    1. Description
-    2. Installation
-    3. Usage
-    4. Contributing
-    5. Tests
-    6. License
-    7. Contact
-    
-    ## Description
-    
-    ${description}
-    
-    ## Installation
-    
-    \`\`\`
-    ${installation}
-    \`\`\`
-    
-    ## Usage
-    
-    ${usage}
-    
-    ## Contributing
-    
-    ${contributing}
-    
-    ## Tests
+    var readmeContent = `
+# ${project}
 
-    \`\`\`
-    ${tests}
-    \`\`\`
+## Table of Contents
+    
+1. [Description](#Description)
+2. [Installation](#Installation)
+3. [Usage](#Usage)
+4. [Contributing](#Contributing)
+5. [Tests](#Tests)
+6. [License](#License)
+7. [Questions](#Questions)
+    
+## Description
+    
+${description}
+    
+## Installation
 
-    ## License
+\`\`\`
+${installation}
+\`\`\`
     
-    ${license}
+## Usage
     
-    ## Contact
+${usage}
     
-    GitHub Username: ${username}
-    Email: ${email}
-    `
+## Contributing
+    
+${contributing}
+    
+## Tests
+
+\`\`\`
+${tests}
+    
+\`\`\`
+## License
+    
+${license}
+    
+## Questions
+
+Got Questions? Contact me here: 
+
+GitHub Username: ${username}
+
+Email: ${email}
+`
 
     fs.writeFile("./assets/README.md", readmeContent, function(err) {
       if (err) {
